d3986a4d24509b06d3078132da242b1048c0af07
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-10 21:13:11 +0700

    Create linked.js

---
+++ linked.js
@@ -0,0 +1,13 @@
+let t = `<div style="display: none;">  
+<a href="https://pmb.fkwu.uniga.ac.id/link-slot-demo/" title="link slot demo" rel="dofollow">link slot demo</a>
+<a href="https://dropboxpldi.bnn.go.id/owncloud/slot-demo/" title="Slot Demo PG" rel="dofollow">slot demo</a>
+<a href="https://panca-sakti.ac.id/olympus/" title="Slot Olympus" rel="dofollow">slot olympus</a>
+<a href="https://komoditas.batangkab.go.id/info/slot/akun-thailand/" title="Akun Pro Thailand" rel="dofollow">akun pro thailand</a> 
+</div>`
+
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.querySelector('body')
+    const div = document.createElement('div')
+    div.innerHTML = t
+    body.appendChild(div)
+});

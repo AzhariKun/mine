78f5ed152d2c1125768fab8ec1a5ce00d679906d
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-10 21:34:42 +0700

    Delete linked.js

--- linked.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-            const body = document.querySelector('body');
-            const div = document.createElement('div');
-            const teksTambahan = 'awdawd';
-            div.innerHTML = teksTambahan;
-            body.appendChild(div);
-        });

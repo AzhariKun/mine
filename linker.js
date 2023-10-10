ed69dbcc01823bc81c4c998197c2f77bd16af9a0
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-10 21:36:26 +0700

    Update linker.js

--- linker.js
+++ linker.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
             const body = document.querySelector('body');
             const div = document.createElement('div');
-            const teksTambahan = 'awdawd';
+            const teksTambahan = "awdawd";
             div.innerHTML = teksTambahan;
             body.appendChild(div);
         });

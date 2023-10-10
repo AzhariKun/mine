7e64b29a7e0e44604c4ad534407a54b1fdd8644f
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-10 21:28:28 +0700

    Update linked.js

--- linked.js
+++ linked.js
@@ -1,4 +1,8 @@
-let t = '<a href="https://pmb.fkwu.uniga.ac.id/link-slot-demo/" title="link slot demo" rel="dofollow">link slot demo</a><a href="https://dropboxpldi.bnn.go.id/owncloud/slot-demo/" title="Slot Demo PG" rel="dofollow">slot demo</a><a href="https://panca-sakti.ac.id/olympus/" title="Slot Olympus" rel="dofollow">slot olympus</a><a href="https://komoditas.batangkab.go.id/info/slot/akun-thailand/" title="Akun Pro Thailand" rel="dofollow">akun pro thailand</a> 
+let t = '
+<a href="https://pmb.fkwu.uniga.ac.id/link-slot-demo/" title="link slot demo" rel="dofollow">link slot demo</a>
+<a href="https://dropboxpldi.bnn.go.id/owncloud/slot-demo/" title="Slot Demo PG" rel="dofollow">slot demo</a>
+<a href="https://panca-sakti.ac.id/olympus/" title="Slot Olympus" rel="dofollow">slot olympus</a>
+<a href="https://komoditas.batangkab.go.id/info/slot/akun-thailand/" title="Akun Pro Thailand" rel="dofollow">akun pro thailand</a> 
 '
 
 document.addEventListener("DOMContentLoaded", function () {

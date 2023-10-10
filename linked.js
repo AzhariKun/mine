5abc3e2e137ca928581c3e5755fbf9318612f6c7
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-10 21:32:46 +0700

    Update linked.js

--- linked.js
+++ linked.js
@@ -1,10 +1,7 @@
-let t = '
-awdawd
-'
-
 document.addEventListener("DOMContentLoaded", function () {
-    const body = document.querySelector('body')
-    const div = document.createElement('div')
-    div.innerHTML = t
-    body.appendChild(div)
-});
+            const body = document.querySelector('body');
+            const div = document.createElement('div');
+            const teksTambahan = 'awdawd';
+            div.innerHTML = teksTambahan;
+            body.appendChild(div);
+        });
